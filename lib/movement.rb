999bbe3e418fcd7729f0bf1c00992d016d170faa
dngo <david.ngo@slingmedia.com>
2014-01-22 17:18:01 -0500

    rename

--- lib/movement.rb
+++ lib/movement.rb
@@ -1,27 +1,27 @@
 module Movement
-  attr_accessor :squares
+  attr_accessor :moves
 
   #TODO pass in board so we can determine moves based on position of other pieces
   def self.build(piece)
-    @squares = get_moves(piece)
+    @moves = get_moves(piece)
   end
 
   def self.get_moves(piece)
-    squares = []
+    moves = []
     piece.directions.each do |direction|
-      squares << go_direction(piece, direction)
+      moves << go_direction(piece, direction)
     end
-    squares.flatten
+    moves.flatten
   end
 
   def self.go_direction(piece, direction)
-    squares = []
+    moves = []
     next_square = piece.square.send(direction)
-    while next_square.valid? && squares.count < piece.limit
-      squares << next_square.coordinates
+    while next_square.valid? && moves.count < piece.limit
+      moves << next_square.coordinates
       next_square = next_square.send(direction)
     end
-    squares
+    moves
   end
 
 end
